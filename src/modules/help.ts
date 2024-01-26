30d09e2a731efbe953c31e36c79f5eef906ec01f
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-01-26 19:12:10 +0530

    Update help.ts

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -8,7 +8,7 @@ composer.callbackQuery("helper", async (ctx) => {
   try {
     await ctx.editMessageText(
       ctx.t("help") +
-        "\n\nTo approve members who are already in waiting list, upgrade to premium! Contact @xditya_bot for information on pricing.",
+        "\n\nAdd me to your channel as administrator with add users permission and forward me a message from that chat to set me up!.",
       {
         reply_markup: new InlineKeyboard()
           .text("Add me to your channel", "add_to_channel")
